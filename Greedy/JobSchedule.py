e2647d821c816da8f28e823a60860f2c2d99e0fa
Chetan Gupta <chetangupta2708@gmail.com>
2023-08-17 14:06:44 +0530

    Initial commit

--- Greedy/JobSchedule.py
+++ Greedy/JobSchedule.py
@@ -1,15 +1,17 @@
 def customJobScheduling(jobs, time_slots):
     n = len(jobs)
-    jobs.sort(key=lambda x: x[1], reverse=True)
+    jobs.sort(key=lambda x: x[1], reverse=True)  #NlogN
     result = [False] * time_slots
     total_profit = 0
-
+#balajiit88@gmail.com
     for i in range(n):
-        for j in range(min(time_slots - 1, jobs[i][0] - 1), -1, -1):
+        for j in range(min(time_slots - 1, jobs[i][0] - 1), -1, -1):   # N2
             if result[j] == False:
                 result[j] = True
-                total_profit += jobs[i][1]  #adding profit
-           
+                total_profit += jobs[i][1]  #adding profit 
+                print(result)
+                print(jobs[i][1])
+                print(total_profit)
                 break
     return total_profit
 
