7dcf1bacea3b944005f7a31f23263becba502b51
Abhishek Dogra <abhidograaa19@gmail.com>
2023-08-18 23:16:37 +0530

    X Pattern Name

--- Patterns/nameInXPattern.py
+++ Patterns/nameInXPattern.py
@@ -2,4 +2,17 @@ str = "Abhishek"
 n = len(str)
 
 for i in range(1, n+1):
-    pow
+    if i <= n/2+1:
+        for k in range(i-1):
+            print(" ", end="")
+    else:
+        for k in range(n-(i-1)):
+            print(" ", end="")
+    print(str[i-1], end="")
+    if (i <= n/2+1):
+        for j in range(n-2*(i-1)):
+            print(" ", end="")
+    else:
+        for j in range(2*(i-int(n/2)-1)):
+            print(" ", end="")
+    print(str[i-1])
